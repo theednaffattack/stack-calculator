abaf4393afc351565e0d98eb65f0421d855e963c
theednaffattack <eddienaff@gmail.com>
2018-03-04 07:35:16 -0800

    Add layout and basic function

--- Main.js
+++ Main.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Button from './Button';
-import { pressNum } from './modules';
+import { pressNum, enter, operation, clear, swap, toggleNegative } from './modules';
+
+const baseNumber = {
+  backgroundColor: '#424242',
+  textAlign: 'right',
+  padding: 10,
+  fontSize: 30,
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: 'papayawhip',
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
   top: {
     paddingTop: 20,
@@ -18,14 +22,13 @@ const styles = StyleSheet.create({
   bottom: {
     flex: 1,
   },
-  number: {
+  number: {},
+  append: {
     color: '#fff',
-    backgroundColor: '#424242',
-    textAlign: 'right',
-    padding: 10,
-    fontSize: 30,
-    fontWeight: 'bold',
+    ...baseNumber,
   },
+  push: { color: '#9bc23c', ...baseNumber },
+  replace: { color: '#2e71e5', ...baseNumber },
   textBottomBorder: {
     borderBottomWidth: 1,
     borderColor: '#fff',
@@ -38,59 +41,78 @@ const styles = StyleSheet.create({
   },
 });
 
-const Main = ({ currentNumber, pressNumWithDispatch }) => (
+const Main = ({
+  calculatorState: { stack, inputState },
+  pressNumWithDispatch,
+  enterAction,
+  operationAction,
+  clearAction,
+  swapAction,
+  toggleNegativeAction,
+}) => (
   <View style={styles.container}>
     <View style={styles.top}>
       <View style={styles.textBottomBorder}>
-        <Text style={styles.number}>0</Text>
+        <TouchableOpacity onPress={() => toggleNegativeAction(2)}>
+          <Text style={styles.append}>{stack[2] || '0'}</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.textBottomBorder}>
-        <Text style={styles.number}>1</Text>
+        <TouchableOpacity onPress={() => toggleNegativeAction(1)}>
+          <Text style={styles.append}>{stack[1] || '0'}</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.textBottomBorder}>
-        <Text style={styles.number}>{currentNumber}</Text>
+        <TouchableOpacity onPress={() => toggleNegativeAction(0)}>
+          <Text style={styles[inputState]}>{stack[0] || '0'}</Text>
+        </TouchableOpacity>
       </View>
     </View>
     <View style={styles.bottom}>
       <View style={styles.row}>
-        <Button text="clear" special specialWord />
-        <Button text="power" specialWord />
-        <Button text="swap" specialWord />
-        <Button text="/" />
+        <Button text="clear" onPress={clearAction} special specialWord />
+        <Button text="power" onPress={operationAction} specialWord />
+        <Button text="swap" onPress={swapAction} specialWord />
+        <Button text="/" onPress={operationAction} />
       </View>
       <View style={styles.row}>
         <Button text="9" onPress={pressNumWithDispatch} />
         <Button text="8" onPress={pressNumWithDispatch} />
         <Button text="7" onPress={pressNumWithDispatch} />
-        <Button text="x" />
+        <Button text="x" onPress={operationAction} />
       </View>
       <View style={styles.row}>
-        <Button text="6" />
-        <Button text="5" />
-        <Button text="4" />
-        <Button text="-" />
+        <Button text="6" onPress={pressNumWithDispatch} />
+        <Button text="5" onPress={pressNumWithDispatch} />
+        <Button text="4" onPress={pressNumWithDispatch} />
+        <Button text="-" onPress={operationAction} />
       </View>
       <View style={styles.row}>
-        <Button text="3" />
-        <Button text="2" />
-        <Button text="1" />
-        <Button text="+" />
+        <Button text="3" onPress={pressNumWithDispatch} />
+        <Button text="2" onPress={pressNumWithDispatch} />
+        <Button text="1" onPress={pressNumWithDispatch} />
+        <Button text="+" onPress={operationAction} />
       </View>
       <View style={styles.row}>
-        <Button text="0" />
-        <Button text="." />
-        <Button text="enter" special enterButton />
+        <Button text="0" onPress={pressNumWithDispatch} />
+        <Button text="." onPress={pressNumWithDispatch} />
+        <Button text="enter" onPress={enterAction} special enterButton />
       </View>
     </View>
   </View>
 );
 
 export default connect(
-  state => ({ currentNumber: state }),
+  state => ({ calculatorState: state }),
   dispatch =>
     bindActionCreators(
       {
         pressNumWithDispatch: pressNum,
+        enterAction: enter,
+        operationAction: operation,
+        clearAction: clear,
+        swapAction: swap,
+        toggleNegativeAction: toggleNegative,
       },
       dispatch,
     ),
--- modules.js
+++ modules.js
@@ -8,7 +8,36 @@ quick reminder on `action` structure:
 }
 */
 
+// local functions
+const doOperation = (x, y, op) => {
+  const a = parseFloat(x);
+  const b = parseFloat(y);
+  if (op === 'power') {
+    return b ** a;
+  } else if (op === '+') {
+    return b + a;
+  } else if (op === '-') {
+    return b - a;
+  } else if (op === 'x') {
+    return b * a;
+  } else if (op === '/') {
+    return b / a;
+  }
+  return 0;
+};
+
+const switchNegative = (num) => {
+  const coerceMe = -Number(num);
+  return coerceMe;
+};
+
+// constants
 const PRESS_NUM = 'PRESS_NUM';
+const ENTER = 'ENTER';
+const OPERATION = 'OPERATION';
+const CLEAR = 'CLEAR';
+const SWAP = 'SWAP';
+const TOGGLE_NEGATIVE = 'TOGGLE_NEGATIVE';
 
 // action
 export const pressNum = n => ({
@@ -16,10 +45,80 @@ export const pressNum = n => ({
   payload: n,
 });
 
-export const reducer = (state = '0', { type, payload }) => {
+export const enter = () => ({
+  type: ENTER,
+});
+
+export const operation = op => ({
+  type: OPERATION,
+  payload: op,
+});
+
+export const clear = () => ({
+  type: CLEAR,
+});
+
+export const swap = () => ({
+  type: SWAP,
+});
+
+export const toggleNegative = idx => ({
+  type: TOGGLE_NEGATIVE,
+  payload: idx,
+});
+
+// reducer
+const initialState = { stack: [], inputState: 'replace' };
+// button press states: inputState = append | replace | push
+
+export const reducer = (state = { stack: [], inputState: 'replace' }, { type, payload }) => {
   switch (type) {
+    case TOGGLE_NEGATIVE:
+      return {
+        stack: state.stack.map((x, i) => (payload === i ? switchNegative(x) : x)),
+        inputState: state.inputState,
+      };
+    case SWAP:
+      return {
+        stack: [state.stack[1], state.stack[0], ...state.stack.slice(2)],
+        inputState: 'push',
+      };
+    case CLEAR:
+      return initialState;
+    case OPERATION:
+      return {
+        stack: [`${doOperation(state.stack[0], state.stack[1], payload)}`, ...state.stack.slice(2)],
+        inputState: 'push',
+      };
+    case ENTER:
+      return {
+        ...state,
+        inputState: 'replace',
+        stack: ['0', ...state.stack],
+      };
     case PRESS_NUM:
-      return state + payload;
+      if (state.inputState === 'append') {
+        // something
+        return {
+          ...state,
+          stack: [(state.stack[0] || '0') + payload, ...state.stack.slice(1)],
+          inputState: 'append',
+        };
+      } else if (state.inputState === 'replace') {
+        console.log('peek at payload\n', payload);
+        return {
+          ...state,
+          stack: [payload, ...state.stack.slice(1)],
+          inputState: 'append',
+        };
+      } else if (state.inputState === 'push') {
+        return {
+          ...state,
+          stack: [payload, ...state.stack],
+          inputState: 'append',
+        };
+      }
+      break;
     default:
       return state;
   }
